1499f31b9fbcf3e3f7e6edc104baba2342297220
rishabh <rishabh.sisodiya4@gmail.com>
2018-12-29 20:58:54 +0530

    Second

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ app.use((req,res,next) => {
 } )
 
 app.use((req,res,next) => {
-  res.render('maintenance.hbs')
+  // res.render('maintenance.hbs')
+  next();
 })
 
 app.use(express.static(__dirname + '/public')); //Use  middleware function to use static files
